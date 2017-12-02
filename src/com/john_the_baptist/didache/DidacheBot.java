e407121f8ecfa0aaf528cc1b9105183e34cb23dc
Banská Bystrica <34194316+bbystrica@users.noreply.github.com>
2017-12-02 16:10:35 -0500

    Set bot's game status
    Sets the bot's game status to "d!help".

--- src/com/john_the_baptist/didache/DidacheBot.java
+++ src/com/john_the_baptist/didache/DidacheBot.java
@@ -7,6 +7,7 @@ import com.john_the_baptist.didache.books.Bible;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.Game;
 import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.exceptions.RateLimitedException;
 
@@ -15,13 +16,14 @@ public class DidacheBot {
 	public static final String DISCORD_AUTH_KEY = "";
 	public static final String BIBLES_AUTH_KEY = "";
 	
-	
 	public static User user;
 
 	public static void main(String[] args) throws LoginException, IllegalArgumentException, InterruptedException, RateLimitedException {
 		
 		JDA jda = new JDABuilder(AccountType.BOT).setToken(DISCORD_AUTH_KEY).buildBlocking();
 		user = jda.getSelfUser();
+		
+		jda.getPresence().setGame(Game.playing("d!help"));
 		
 		jda.addEventListener(new MessageListener());
 	}
